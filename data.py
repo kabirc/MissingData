a690a4e6a2da28b68683c1676a0260b067e7cc8f
Kabir Chandrasekher <kabirc@DN525api.SUNet>
2019-03-14 14:35:47 -0700

    Initial commit to provide structure

---
+++ data.py
@@ -0,0 +1,23 @@
+class DataMatrix:
+    """
+    Abstract data type to store data matrices.  Different assumptions on data
+    inherit from this class.
+    """
+    def __init__(self):
+        # TODO
+
+    def get_X(self):
+        # TODO
+
+    def get_X_hat(self):
+        # TODO
+
+class DiagonalData(DataMatrix):
+    """
+    Simplest example of data matrix.  Gaussian with diagonal covariance matrix.
+    """
+    def __init__(self):
+        # TODO
+
+    def get_X_hat(self):
+        # TODO
---
+++ imputation.py
@@ -0,0 +1,10 @@
+class ImputationMissingData:
+
+    def __init__(self):
+        # TODO
+
+    def generate_data(self):
+        # TODO
+
+    def simulate(self):
+        # TODO
